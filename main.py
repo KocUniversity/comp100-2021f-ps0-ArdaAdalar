e4da4d12463421bc8f98cfc38a69528e068eaf06
ArdaAdalar <91716301+ArdaAdalar@users.noreply.github.com>
2021-10-01 07:58:34 +0000

    Raising a Number to a Power and Taking a Logarithm

---
+++ main.py
@@ -0,0 +1,11 @@
+language = "python3" 
+run = "python main.py"
+x = input("Enter number x:")
+y = input("Enter number y:")
+x=2
+y=3
+import math
+print("x**y =" , math.pow(x, y))
+import math
+print("log(x)=" , math.log2(x))
+print ("76188")
